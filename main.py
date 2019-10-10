8164e85e0bd3f1df81d47c0696b46edaab6b0de7
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-10-10 23:15:39 +1100

    Validation

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 import pandas as pd 
 import numpy as np 
 
@@ -22,22 +23,32 @@ print(raw_pulsar_data.describe(), "\n")
 print("--- FEATURES ---")
 print(raw_pulsar_data.columns, "\n")
 
-features = [ "Mean of the integrated profile",
-      "Standard deviation of the integrated profile",
-      "Excess kurtosis of the integrated profile",
-      "Skewness of the integrated profile", 
-      "Mean of the DM-SNR curve",
-      "Standard deviation of the DM-SNR curve",
-      "Excess kurtosis of the DM-SNR curve", 
-      "Skewness of the DM-SNR curve"]
+features = [" Mean of the integrated profile",
+      " Standard deviation of the integrated profile",
+      " Excess kurtosis of the integrated profile",
+      " Skewness of the integrated profile", 
+      " Mean of the DM-SNR curve",
+      " Standard deviation of the DM-SNR curve",
+      " Excess kurtosis of the DM-SNR curve", 
+      " Skewness of the DM-SNR curve"]
 
 # Splitting data set into training
 # and test data
 X = raw_pulsar_data[features]
 y = raw_pulsar_data["target_class"]
 
-train_X, val_X, train_y, val_y = train_test_split(X, y, random_state=1)
+train_X, test_X, train_y, test_y = train_test_split(X, y, random_state=1)
 
 # Creating a Random Forest Regressor Model
 rf_model = RandomForestRegressor(random_state=1)
 rf_model.fit(train_X, train_y)
+
+# Model Validation
+test_pred = rf_model.predict(test_X)
+
+mae = mean_absolute_error(test_pred, test_y)
+print("Mean Absolute Error:", mae)
+
+# Outputting first couple of rows
+print(test_pred[:5])
+print(test_y[:5])
