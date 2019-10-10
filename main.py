2db618032cd70ddc84765d57ef77e25f96cdf930
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-10-10 22:49:23 +1100

    Program now outputs basic data visualization

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from sklearn.ensemble import RandomForestRegressor
 import pandas as pd 
 import numpy as np 
+
+raw_pulsar_data = pd.read_csv("pulsar_stars.csv")
+
+print("--- SAMPLE DATA ---")
+print(raw_pulsar_data.head(), "\n")
+
+print("--- DATA INFO ---")
+print(raw_pulsar_data.describe(), "\n")
+
+print("--- FEATURES ---")
+print(raw_pulsar_data.columns)
