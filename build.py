159a964d32667986a73f806a1f18fc95f27e1a62
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-10-18 13:52:17 +1100

    Changes to final build script

--- build.py
+++ build.py
@@ -9,11 +9,25 @@ from sklearn.metrics import mean_absolute_error
 from sklearn.metrics import r2_score
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.neural_network import MLPRegressor
+from joblib import dump, load
 import pandas as pd 
 import numpy as np 
 import sys
 
+NN_FNAME = "Pulsar_Stars_Neural_Network.joblib"
+KN_FNAME = "Pulsar_Stars_K_Neighbours.joblib"
+RF_FNAME = "Pulsar_Stars_Random_Forest.joblib"
+
 raw_pulsar_data = pd.read_csv("pulsar_stars.csv")
+
+features = [" Mean of the integrated profile",
+      " Standard deviation of the integrated profile",
+      " Excess kurtosis of the integrated profile",
+      " Skewness of the integrated profile", 
+      " Mean of the DM-SNR curve",
+      " Standard deviation of the DM-SNR curve",
+      " Excess kurtosis of the DM-SNR curve", 
+      " Skewness of the DM-SNR curve"]
 
 # Splitting data set into training
 # and test data
@@ -37,116 +51,21 @@ def build_specified_model(model):
 
 def build_neural_network_model():
   print("\n--- CREATING NEURAL NETWORK MODEL ---") 
-  # Creating a Neural Network MLP Regressor Model
-  mlp_model = MLPRegressor(random_state=1)
-  mlp_model.fit(train_X, train_y)
-
-  # Model Validation
-  test_pred = np.round(abs(mlp_model.predict(test_X)))
-
-  mae = mean_absolute_error(test_pred, test_y)
-  r2 = r2_score(test_pred, test_y)
-
-  print("\n" + "Mean Absolute Error:", mae)
-  print("R2 Score:", r2) 
-
-  # Outputting first couple of rows
-  print(test_pred[:5])
-  print(test_y[:5])
-
   hidden_layer_size = 1000
   learning_rate = "constant"
   learning_rate_init = 0.1
-
+  max_iter_size = 5000
+  solver = "lbfgs"
   activation_algorithm = "tanh"
-  # Model Improvement
-  # Creating discrete hyperparameter amounts to trial
-  print("\n--- BEGINNING MODEL IMPROVEMENTS ---")
-  solvers = ["adam", "lbfgs"]
-  max_iter_nums = [200, 500, 1000, 5000]
-  cand_hidden_layer_sizes = [5, 10, 50, 100, 500, 1000]
-  activation_algorithms = ["identity", "logistic", "tanh", "relu"]
-  learning_rate_inits = [0.0001, 0.001, 0.0015, 0.002, 0.005, 0.01, 0.1]
-  learning_rates = ["constant", "invscaling", "adaptive"]
 
-  print("\n--- ADJUSTING HIDDEN LAYER SIZES ---")
-
-  best_hidden_layer_sizes_data = [0, 0]
-  for hidden_layer_size in cand_hidden_layer_sizes:
-    mlp_model = MLPRegressor(hidden_layer_sizes=hidden_layer_size, random_state=1)
-    mlp_model.fit(train_X, train_y)
-    preds = np.round(abs(mlp_model.predict(test_X)))
-    score = r2_score(preds, test_y)  
-    print("\nHidden Layer Sizes:", hidden_layer_size)
-    print("R2 Score:", score)
-    if score > best_hidden_layer_sizes_data[1]:
-      best_hidden_layer_sizes_data = [hidden_layer_size, score]
-
-  best_hidden_layer_size = best_hidden_layer_sizes_data[0]
-  print("Optimal hidden layer size:", best_hidden_layer_size)
-
-  print("\n--- ADJUSTING LEARNING RATE ---")
-
-  best_learning_rate_data = ["", 0, 0]
-  for learning_rate in learning_rates:
-    for learning_rate_init in learning_rate_inits:
-      mlp_model = MLPRegressor(learning_rate=learning_rate, learning_rate_init=learning_rate_init, random_state=1)
-      mlp_model.fit(train_X, train_y)
-      preds = np.round(abs(mlp_model.predict(test_X)))
-      score = r2_score(preds, test_y)
-      print("\nLearning Rate:", learning_rate)
-      print("Initial Learning Rate:", learning_rate_init)
-      print("R2 Score:", score)
-      if score > best_learning_rate_data[2]:
-        best_learning_rate_data = [learning_rate, learning_rate_init, score]
-
-  best_learning_rate = best_learning_rate_data[0]
-  best_learning_rate_init = best_learning_rate_data[1]
-  print("\nOptimal learning rate:", best_learning_rate)
-  print("Optimal initial learning rate:", best_learning_rate_init) 
-
-  print("\n--- ADJUSTING ACTIVATION ALGORITHM ---")
-
-  best_activation_algorithm_data = ["", 0]
-  for activation_algorithm in activation_algorithms:
-    mlp_model = MLPRegressor(activation=activation_algorithm, random_state=1)
-    mlp_model.fit(train_X, train_y)
-    preds = np.round(abs(mlp_model.predict(test_X)))
-    score = r2_score(preds, test_y)
-    print("\nActivation Algorithm:", activation_algorithm)
-    print("R2 Score:", score)
-    if score > best_activation_algorithm_data[1]:
-      best_activation_algorithm_data = [activation_algorithm, score]
-
-  best_activation_algorithm = best_activation_algorithm_data[0]
-  print("\nOptimal activation algorithm:", best_activation_algorithm)
-
-  print("\n--- ADJUSTING MAXIMUM ITERATIONS WITH VARYING SOLVERS ---")
-
-  best_solver_data = [0, 0, 0]
-  for solver in solvers:
-    for max_iter_num in max_iter_nums:
-      mlp_model = MLPRegressor(solver=solver, max_iter=max_iter_num, random_state=1)
-      mlp_model.fit(train_X, train_y)
-      preds = np.round(abs(mlp_model.predict(test_X)))
-      score = r2_score(preds, test_y)
-      print("\nSolver:", solver)
-      print("Max Iterations:", max_iter_num)
-      print("R2 Score:", score)
-      if score > best_solver_data[2]:
-        best_solver_data = [solver, max_iter_num, score]
-
-  best_solver = best_solver_data[0]
-  best_max_iter = best_solver_data[1]
-  print("\nOptimal amount of maximum iterations:", best_max_iter)
-  print("Solver:", best_solver)
-
-  print("--- CREATING FINAL MODEL ---")
-  model = MLPRegressor(activation=best_activation_algorithm, max_iter=best_max_iter, solver=best_solver, learning_rate=best_learning_rate, learning_rate_init=best_learning_rate_init, hidden_layer_sizes=best_hidden_layer_size)
-  model.fit(train_X, train_y)
-  preds = model.predict(test_X)
+  mlp_model = MLPRegressor(hidden_layer_sizes=hidden_layer_size, learning_rate=learning_rate, learning_rate_init=learning_rate_init, max_iter=max_iter_size, solver=solver, activation=activation_algorithm, random_state=1)
+  mlp_model.fit(train_X, train_y)
+  preds = np.round(abs(mlp_model.predict(test_X)))
   score = r2_score(preds, test_y)
-  print("R2 Score:", score)
+
+  print("Scoring of final neural network:", score)
+  print("DUMPING MODEL TO THE FILE", NN_FNAME)
+  dump(mlp_model, NN_FNAME)
 
 def build_k_nearest_neighbours_model():
   print("\n--- CREATING K NEAREST NEIGHBOURS REGRESSOR MODEL ---") 
